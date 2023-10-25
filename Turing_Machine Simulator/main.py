131983065b63211c3566c7c302854d646486c6e5
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2023-10-25 14:37:18 +0530

    Update main.py

--- Turing_Machine Simulator/main.py
+++ Turing_Machine Simulator/main.py
@@ -263,8 +263,7 @@ class TuringMachineSimulator(tk.Tk):
 			'Helvetica', 10), fg='black', cursor='hand2') 
 		made_by_label.pack(side='bottom', pady=10) 
 		made_by_label.bind( 
-			'<Button-1>', lambda e: webbrowser.open_new("https:"
-							+"//www.geeksforgeeks.org/")) 
+			'<Button-1>', lambda e: webbrowser.open_new("http://www.sruthi-sivasankararaj.com/")) 
 
 
 if __name__ == '__main__': 
